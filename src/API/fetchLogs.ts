51adfa3b01b8778214980dcca2ad40c854e483e1
Matt Howard <moward@users.noreply.github.com>
2023-03-07 10:54:01 -0500

    Fit fetchLogs

--- src/API/fetchLogs.ts
+++ src/API/fetchLogs.ts
@@ -39,7 +39,6 @@ export default async function fetchLogs(
           ts,
           level: LogLevel.Debug,
           msg: "200 GET /healthz (172.17.0.1) 0.85ms",
-          replica: "replica-1",
         });
       }
     }
@@ -50,7 +49,6 @@ export default async function fetchLogs(
           ts,
           level: LogLevel.Debug,
           msg: "200 GET /healthz (172.17.0.1) 0.85ms",
-          replica: "replica-2",
         });
       }
     }
@@ -62,7 +60,6 @@ export default async function fetchLogs(
           ts,
           level: LogLevel.Info,
           msg: "Will fork 1 workers",
-          replica: "replica-1",
         });
       }
     }
@@ -73,73 +70,61 @@ export default async function fetchLogs(
         ts,
         level: LogLevel.Error,
         msg: "Caught exception in:",
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: '  File "/home/runner/work/baseten/baseten/.venv/lib/python3.9/site-packages/promise/promise.py", line 489, in _resolve_from_executor',
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: "    executor(resolve, reject)",
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: '  File "/home/runner/work/baseten/baseten/.venv/lib/python3.9/site-packages/promise/promise.py", line 756, in executor',
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: "    return resolve(f(*args, **kwargs))",
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: '  File "/home/runner/work/baseten/baseten/.venv/lib/python3.9/site-packages/graphql/execution/middleware.py", line 75, in make_it_promise',
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: "    return next(*args, **kwargs)",
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: '  File "/home/runner/work/baseten/baseten/backend/users/services.py", line 88, in wrap_function',
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: "    return function(*args, **kwargs)",
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: '  File "/home/runner/work/baseten/baseten/backend/common/graphql/utils.py", line 170, in func_wrapper',
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: "    raise GraphQLResourceNotValidatedError(e)",
-        replica: "replica-1",
       });
       logs.push({
         ts,
         level: LogLevel.Error,
         msg: "graphql.error.located_error.GraphQLLocatedError: File is too large. It must be at most 4gb.",
-        replica: "replica-1",
       });
     }
   }
