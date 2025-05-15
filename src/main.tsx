13f97638c1b43da75648beff8dbdc7ab1665759c
Sidharth Shanker <sid.shanker@baseten.co>
2025-05-15 11:27:04 -0400

    Modernize. (#2)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import "./index.css";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './App.css';
 
-ReactDOM.render(
-  <React.StrictMode>
+import App from './App';
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
